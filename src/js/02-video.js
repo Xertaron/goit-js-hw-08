b7a74cf8e6e580148b50d1a31823cfe7f161157f
Xertaron <xertaron@gmail.com>
2023-06-18 16:37:01 +0200

    coś sie może zadzieje

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const video = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const time = 'videoplayer-current-time';
+
+const currentLength = function (length) {
+  const seconds = length.seconds;
+  localStorage.setItem(time, JSON.stringify(seconds));
+};
+
+video.on('timeupdate', throttle(getCurrentTime, 500));
+
+video.setCurrentTime(JSON.parse(localStorage.getItem(time)) || 0);
+
+video
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occured while setting the color');
+  });
