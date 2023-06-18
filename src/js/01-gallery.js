dbefa2f3b2e83aa69f71df313e0562ee3e0586a3
Xertaron <xertaron@gmail.com>
2023-06-18 15:09:06 +0200

    niech to zadziała

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const galleryBox = document.querySelector('.gallery');
+const markup = newGalleryItems(galleryItems);
+galleryBox.insertAdjacentHTML('afterbegin', markup);
+
+function newGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class = "gallery__item" href="${original}"><img class = "gallery__image" src="${preview}" alt=${description}/></a>`;
+    })
+    .join('');
+}
+const newLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
